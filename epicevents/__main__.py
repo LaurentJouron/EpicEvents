0f4463be8570efb40059b257653a139dfa44ccea
Laurent Jouron <jouronlaurent@hotmail.com>
2024-02-02 18:01:19 +0100

    push for office

--- epicevents/__main__.py
+++ epicevents/__main__.py
@@ -2,7 +2,6 @@ from .database import Model, engine
 from .views.home_views import ExitView
 from .controllers import ReceptionController
 import sentry_sdk
-import logging
 from sentry_sdk.integrations.logging import LoggingIntegration
 import typer
 
@@ -32,13 +31,17 @@ def main():
         ...
         Goodbye! Thank you for using EpicEvents.
     """
+    # import sentry_sdk
+
     sentry_sdk.init(
-        dsn="https://c22680ef370acc9db6fe3515889cfe7a6a73ef7b633af5ca2a3b2772e44d33d2",
+        dsn="https://44c71e14b86c0aafc7e4d7d2cdf4e7d6@us.sentry.io/4506677090189312",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
         traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
         profiles_sample_rate=1.0,
-        enable_tracing=True,
-        debug=False,
-        environment="development",
     )
     Model.metadata.create_all(engine)
     controller = ReceptionController()
